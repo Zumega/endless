5944bf55d73a6dfcc07e0265f11e5d9fd8012f58
Ross <rpm1977@gmail.com>
2018-07-24 19:05:14 -0700

    site BORKED for now

--- src/components/Content/Indexes/IndexesContainer.js
+++ src/components/Content/Indexes/IndexesContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import GenresContainer from '../Stories/Genres/GenresContainer';
 // import PropTypes from 'prop-types';
 
 class IndexesContainer extends Component {
 
   render() {
+    // TODO needs a lot of work!
     return <GenresContainer genres={genres} handleGenre={this.handleGenre} />;
   }
 }
